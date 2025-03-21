a2189807d821cc0ba7887709666a3995e1a807d5
bnszky <michal.banaszkiewicz.pvt@gmail.com>
2025-03-21 20:36:42 +0100

    changed link Urls

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -2,6 +2,6 @@ export const links = {
     about: '/',
     cocktails: '/search',
     cocktail: '/drink/:id',
-    github: 'https://github.com/bnszky/cocktail-paradise-solvro',
-    email: 'mailto:280455@gmail.com'
+    github: 'https://github.com/bnszky/cocktails-paradise-solvro',
+    email: 'mailto:280455@student.pwr.edu.pl'
 }
